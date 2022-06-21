1a1c402e837fe1d4b43c5460a1ae04a6854d81ca
Nicolas Gutik <ngutik@kovix-group.com>
2022-06-21 18:19:56 -0300

    trabajo terminado

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ const linksMdz = document.getElementsByClassName('linksMdz')[0]
 hamburguesa.addEventListener('click', () => {
     linksMdz.classList.toggle('active')
 })
+
+const modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener('click', function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add('open');
+    const exits = modal.querySelectorAll('.modal-exit');
+    exits.forEach(function (exit) {
+      exit.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.classList.remove('open');
+      });
+    });
+  });
+});
+
+
+
+
+
+function pruebaemail(valor){
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+	if(!re.exec(valor)){
+		alert('email no valido');
+        console.log(valor)
+	}
+	else alert('se ha enviado un correo');
+}
